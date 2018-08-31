73d71c10b929a8905341ecce22796cb11591c954
Leonarjn20 <Leonarjn20@BFHS-382161D.pwcs.edu>
2018-08-31 12:31:08 -0400

    Day "one" working on project

--- ArrayList/src/MyArrayList.java
+++ ArrayList/src/MyArrayList.java
@@ -1,48 +1,96 @@
 import java.util.Collection;
 
+/**
+ * The 
+ * @author Jaden Leonard
+ * @version 08/31/18
+ */
 public class MyArrayList{
 	private int size;
-	private Object [] myArray;
+	private Object [] values;
 	private int maxCapacity;
 	
 
 public MyArrayList() {
     maxCapacity = 8;
     size = 0;
-    myArray = new Object[maxCapacity];
+    values = new Object[maxCapacity];
 }
 
 public MyArrayList(int capacity) {
     maxCapacity = capacity;
     size = 0;
-    myArray = new Object[maxCapacity];
+    values = new Object[maxCapacity];
 }
 
 /* public MymyArrayList(Collection c) {
    //TODO unfinished
     maxCapacity  = c.size();
-    myArray = c.tomyArray();
+    values = c.tomyArray();
     size = c.size();
     
 } */
+
+public boolean add(Object obj) {
+    //TODO finish
+    return true;
+}
+
+public void add( int index, Object obj ) {
+    //TODO finish
+}
+
+public boolean remove(Object obj) {
+    //TODO finish
+    return false;
+}
+
+public Object remove(int index) {
+    //TODO finish
+    return null;
+}
 
 public Object get(int index){
     if  (index < 0 || index >= size()) {
         throw new IndexOutOfBoundsException();
     }
     else {
-        return myArray[index];
+        return values[index];
     }
+}
+
+public void set( int index, Object obj) {
+    //TODO finish
 }
 
 public int indexOf(Object o) {
     for (int i = 0; i < size; i++) {
-        if (myArray[i].equals(o)) {
+        if (values[i].equals(o)) {
             return i;
         }
     }
     return -1;
 }
+
+public int lastIndexOf(Object o) {
+    for (int i = size - 1; i <= 0; i--) {
+        if (values[i].equals(o)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+public boolean contains(Object obj) {
+    for(int i = 0; i < size; i++) {
+        if (obj == values[i]) return true;
+    }
+    return false;
+}
+
+/*private void checkSize(int index) {
+    
+}*/
 
 public int size() {
     return size;
@@ -56,9 +104,20 @@ public void ensureCapacity(int minCapacity) {
     //TODO add the elements
     if (maxCapacity < minCapacity) {
         Object [] rmyArray = new Object[minCapacity];
-        myArray = rmyArray;
+        for(int i = 0; i < values.length; i++) {
+            rmyArray[i] = values[i];
+        }
     }
 }
 
-
+/**
+ * Removes all values of the ArrayList and sets the size of the list to 0.
+ * 
+ */
+public void clear() {
+    for (int i = 0; i < values.length; i++) {
+        values[i] = null;
+    }
+    size = 0;
+    }
 }
