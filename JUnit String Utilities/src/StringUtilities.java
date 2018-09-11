8eef06bb26149eb2fb4e117fee209518a1b5b8f7
Jadennl <jnleonard@hotmail.com>
2018-09-11 19:25:29 -0400

    Added alot

--- JUnit String Utilities/src/StringUtilities.java
+++ JUnit String Utilities/src/StringUtilities.java
@@ -1,182 +1,213 @@
-/** 
+package src;
+
+/**
  * A class that performs simple calculates on a given stored String
  * @author MeermaBR and <Your Name>
  * @version 9/4/18
  */
 public class StringUtilities
 {
-   private String string;
-   
-   /**
-    * Builds an empty (null) StringUtilities
-    */
-   public StringUtilities()
-   {
-       string = null;
-   }
+    private String string;
 
-   /**
-    * Builds a StringUtilities with the given String value
-    * @param string The string to be stored for manipulation
-    */
-   public StringUtilities( String string )
-   {
-       this.string = string;
-   }
-   
-   /**
-    * Changes the stored String
-    * @param string The new value for the String to become
-    */
-   public void setString( String string )
-   {
-       this.string = string;
-   }   
-   
-   /**
-    * Returns the value of the String
-    * @return The value of the String or null if the String is null
-    */
-   public String toString()
-   {
-       return string;
-   }
-   
-   /**
-    * Calculates a reversed value of the stored String
-    * @return A new String with all values reversed
-    * @throws NullPointerException if the original String is null
-    */
-   public String reverse()
-   {
-       if ( string == null )
-       {
-           throw new NullPointerException();
-       }
-       StringBuilder ans = new StringBuilder();
-       for ( int i = string.length() - 1; i >= 0; i-- )
-       {
-           ans.append( string.charAt(i) );
-       }
-       
-       return ans.toString();
-   }
+    /**
+     * Builds an empty (null) StringUtilities
+     */
+    public StringUtilities()
+    {
+        string = null;
+    }
 
-   /**
-    * Calculates a reversed value of a subsection of the String
-    * @param from The index to begin at (inclusive)
-    * @param to The index to stop at (exclusive)
-    * @return A new String with all values reversed between index 
-    * from (inclusive) and to (exclusive)
-    * @throws NullPointerException if the original String is null
-    * @throws IllegalArgumentException if the given indices are not valid
-    */
-   public String reverse(int from, int to)
-   {
-       if (string == null) throw new NullPointerException();
-       if (from < 0 || from >= string.length() || to < 0 || to >= string.length() ) throw new IllegalArgumentException();
-       StringBuilder ans = new StringBuilder();
-       for (int i = to - 1; i >= from; i--) {
-           ans.append(string.charAt(i));
-       }
-       return ans.toString();
-   }
-   
-   /**
-    * Determines if all the alpha-characters in the string are Uppercase
-    * @return True if all alpha-characters in the string are Uppercase
-    * @throws NullPointerException if the original String is null
-    */
-   public boolean isAllUpper()
-   {
-       for(char c : string.toCharArray()) {
+    /**
+     * Builds a StringUtilities with the given String value
+     * @param string The string to be stored for manipulation
+     */
+    public StringUtilities( String string )
+    {
+        this.string = string;
+    }
 
-       }
-       return false;
-   }
+    /**
+     * Changes the stored String
+     * @param string The new value for the String to become
+     */
+    public void setString( String string )
+    {
+        this.string = string;
+    }
 
-   /**
-    * Determines if all the alpha-characters in the string are Lowercase
-    * @return True if all alpha-characters in the string are Lowercase
-    * @throws NullPointerException if the original String is null
-    */
-   public boolean isAllLower()
-   {
-       //TODO Unfinished method, fix return
-       return false;
-   }
+    /**
+     * Returns the value of the String
+     * @return The value of the String or null if the String is null
+     */
+    public String toString()
+    {
+        return string;
+    }
 
-   /**
-    * Determines if any of the characters in the string are numeric (0-9)
-    * @return True if any 0-9 character is in the string
-    * @throws NullPointerException if the original String is null
-    */
-   public boolean containsNumbers()
-   {
-       //TODO Unfinished method, fix return
-       return false;
-   }
-   
-   /**
-    * Determines if the String itself is storing a number.
-    * A number can start with a negative sign (-), may
-    * contains one period (.), and otherwise is made only
-    * of digits from 0-9.
-    * @return True if the entire string is a number
-    * @throws NullPointerException if the original String is null
-    */
-   public boolean isNumber()
-   {
-       //TODO Unfinished method, fix return
-       return false;       
-   }
-   
-   /**
-    * Counts the number of characters which have the same value
-    * next to them. ie: Meow = 0, Moo = 1, Shhhh! = 3 
-    * @return The number of consecutive duplicates found
-    * @throws NullPointerException if the original String is null
-    */
-   public int numConsecutiveDuplicates()
-   {
-       //TODO Unfinished method, fix return
-       return 0;       
-   }
-   
-   /**
-    * Counts the number of times a given char is in the String
-    * @param c The char to be counted
-    * @return The number of times the char is in the String
-    * @throws NullPointerException if the original String is null
-    */
-   public int numMatches( char c )
-   {
-       //TODO Unfinished method, fix return
-       return 0;
-   }
-   
-   /**
-    * Counts the number of times a given String is found in the original
-    * @param other The string to be looked for
-    * @return The number of times other is in the original String
-    * @throws NullPointerException if either the original or other string are null
-    */
-   public int numMatches( String other )
-   {
-       //TODO Unfinished method, fix return
-       return 0;
-   }
+    /**
+     * Calculates a reversed value of the stored String
+     * @return A new String with all values reversed
+     * @throws NullPointerException if the original String is null
+     */
+    public String reverse()
+    {
+        if ( string == null )
+        {
+            throw new NullPointerException();
+        }
+        StringBuilder ans = new StringBuilder();
+        for ( int i = string.length() - 1; i >= 0; i-- )
+        {
+            ans.append( string.charAt(i) );
+        }
 
-   /**
-    * Calculates an array version of the String, with each slot a char 
-    * @return An array conversion of the String
-    * @throws NullPointerException if the String is null.
-    */
-   public char [] asArray()
-   {
-       //TODO Unfinished method, fix return
-       return null;
-   }
+        return ans.toString();
+    }
 
-   
+    /**
+     * Calculates a reversed value of a subsection of the String
+     * @param from The index to begin at (inclusive)
+     * @param to The index to stop at (exclusive)
+     * @return A new String with all values reversed between index
+     * from (inclusive) and to (exclusive)
+     * @throws NullPointerException if the original String is null
+     * @throws IllegalArgumentException if the given indices are not valid
+     */
+    public String reverse(int from, int to)
+    {
+        if (string == null) throw new NullPointerException();
+        if (from < 0 || from >= string.length() || to < 0 || to >= string.length() ) throw new IllegalArgumentException();
+        StringBuilder ans = new StringBuilder();
+        for (int i = to - 1; i >= from; i--) {
+            ans.append(string.charAt(i));
+        }
+        return ans.toString();
+    }
+
+    /**
+     * Determines if all the alpha-characters in the string are Uppercase
+     * @return True if all alpha-characters in the string are Uppercase
+     * @throws NullPointerException if the original String is null
+     */
+    public boolean isAllUpper()
+    {
+        if (string.equals(null)) throw new NullPointerException();
+        return string.equals(string.toUpperCase());
+    }
+
+    /**
+     * Determines if all the alpha-characters in the string are Lowercase
+     * @return True if all alpha-characters in the string are Lowercase
+     * @throws NullPointerException if the original String is null
+     */
+    public boolean isAllLower()
+    {
+        if (string.equals(null)) throw new NullPointerException();
+        return string.equals(string.toLowerCase());
+    }
+
+    /**
+     * Determines if any of the characters in the string are numeric (0-9)
+     * @return True if any 0-9 character is in the string
+     * @throws NullPointerException if the original String is null
+     */
+    public boolean containsNumbers()
+    {
+        if (string.equals(null)) throw new NullPointerException();
+        for (char c : string.toCharArray()){
+            if (Character.isDigit(c)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determines if the String itself is storing a number.
+     * A number can start with a negative sign (-), may
+     * contains one period (.), and otherwise is made only
+     * of digits from 0-9.
+     * @return True if the entire string is a number
+     * @throws NullPointerException if the original String is null
+     */
+    public boolean isNumber()
+    {
+        throwNull();
+        int numPeriods = 0;
+        for (int i = 0; i < string.length(); i++){
+            char c = string.charAt(i);
+            if (i == 0 && !Character.isDigit(c) && c != '-') return false;
+            else if (c == '.'){
+                numPeriods++;
+                if (numPeriods > 1)return false;
+            }
+            else if ( !Character.isDigit(c) )return false;
+        }
+        return true;
+    }
+
+    /**
+     * Counts the number of characters which have the same value
+     * next to them. ie: Meow = 0, Moo = 1, Shhhh! = 3
+     * @return The number of consecutive duplicates found
+     * @throws NullPointerException if the original String is null
+     */
+    public int numConsecutiveDuplicates()
+    {
+        throwNull();
+        int consec = 0;
+        char prev = '\u0000';
+        for (char c : string.toCharArray()){
+            if ( c == prev ) consec++;
+            prev = c;
+        }
+        return consec;
+    }
+
+    /**
+     * Counts the number of times a given char is in the String
+     * @param c The char to be counted
+     * @return The number of times the char is in the String
+     * @throws NullPointerException if the original String is null
+     */
+    public int numMatches( char c )
+    {
+        throwNull();
+        int match = 0;
+        for ( char ch : string.toCharArray()){
+            if ( ch == c ) match++;
+        }
+        return match;
+    }
+
+    /**
+     * Counts the number of times a given String is found in the original
+     * @param other The string to be looked for
+     * @return The number of times other is in the original String
+     * @throws NullPointerException if either the original or other string are null
+     */
+    public int numMatches( String other )
+    {
+        throwNull();
+        //TODO Unfinished method, fix return
+        return 0;
+    }
+
+    /**
+     * Calculates an array version of the String, with each slot a char
+     * @return An array conversion of the String
+     * @throws NullPointerException if the String is null.
+     */
+    public char [] asArray()
+    {
+        if (string == null) throw new NullPointerException();
+        char[] ar = new char[string.length()];
+        for(int i = 0; i < string.length(); i++ ){
+            ar[i] = string.charAt(i);
+        }
+        return ar;
+    }
+
+    private void throwNull(){
+        if (string == null ) throw new NullPointerException();
+    }
+
 }
--- JUnit String Utilities/src/StringUtilitiesTest.java
+++ JUnit String Utilities/src/StringUtilitiesTest.java
@@ -1,9 +1,14 @@
-﻿import static org.junit.Assert.*;
+package src;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * f
+ */
 public class StringUtilitiesTest {
 
     private StringUtilities default1;
@@ -11,70 +16,95 @@ public class StringUtilitiesTest {
     private StringUtilities default3;
     private StringUtilities default4;
     private StringUtilities default5;
-
+    
     @Before
     public void setup()
     {
         default1 = new StringUtilities();
         default2 = new StringUtilities("Hello, World!");
-	default3 = new StringUtilities("");
+        default3 = new StringUtilities("");
+        default4 = new StringUtilities("foobar");
+        default5 = new StringUtilities("vvvvvv");
     }
-
+    
     @After
     public void tearDown()
     {
         default1 = null;
         default2 = null;
+        default3 = null;
+        default4 = null;
+        default5 = null;
+        
     }
-
-
-    @Test(expected=NullPointerException.class)
-    public void testReverseWithNull()
-    {
+    
+    
+    @Test(expected = NullPointerException.class)
+    public void testReverseWithNull() {
         default1.reverse();
     }
 
     @Test
-    public void testReverseWithSingleChar()
-    {
-        StringUtilities s = new StringUtilities( "z" );
-        assertEquals( "z", s.reverse() );
+    public void testReverseWithSingleChar() {
+        StringUtilities s = new StringUtilities("z");
+        assertEquals("z", s.reverse());
 
     }
 
     @Test
-    public void testReverseWithDoubleSameChar()
-    {
-        StringUtilities s = new StringUtilities( "zz" );
-        assertEquals( "zz", s.reverse() );
+    public void testReverseWithDoubleSameChar() {
+        StringUtilities s = new StringUtilities("zz");
+        assertEquals("zz", s.reverse());
 
     }
 
 
     @Test
-    public void testReverseWithDoubleDifferentChar()
-    {
-        StringUtilities s = new StringUtilities( "az" );
-        assertEquals( "za", s.reverse() );
+    public void testReverseWithDoubleDifferentChar() {
+        StringUtilities s = new StringUtilities("az");
+        assertEquals("za", s.reverse());
 
     }
 
     @Test
-    public void testReverseWithRandomStuff()
-    {
-        StringUtilities s = new StringUtilities( "╥æ±B è\n" );
-        assertEquals( "\nè B±æ╥", s.reverse() );
+    public void testReverseWithRandomStuff() {
+        StringUtilities s = new StringUtilities("╥æ±B è\n");
+        assertEquals("\nè B±æ╥", s.reverse());
 
     }
 
     @Test
     public void testReverseWithValidIndexes() {
         StringUtilities s = new StringUtilities("WANABEEE");
-        assertEquals("EBAN", s.reverse(2,6));
+        assertEquals("EBAN", s.reverse(2, 6));
     }
 
     @Test
     public void testReverseWithSameIndexes() {
-        assertEquals("", default2.reverse(0,0));
+        assertEquals("", default2.reverse(0, 0));
     }
+    
+    @Test
+    public void testNumberWithDecimal() {
+        StringUtilities nums = new StringUtilities("1245.5");
+        assertTrue(nums.isNumber());
+    }
+
+    @Test
+    public void testNumberWithNegativeInvalid() {
+        StringUtilities nums = new StringUtilities("45-3");
+        assertFalse(nums.isNumber());
+    }
+    
+    @Test
+    public void testReturnNull() {
+        assertNull( default1.toString() );
+    }
+    
+    @Test
+    public void testCaps(){
+        StringUtilities s = new StringUtilities("WAKE UP !!");
+        assertEquals(true, s.isAllUpper());
+    }
+    
 }
