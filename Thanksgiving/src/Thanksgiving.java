d4422b7bdb63050a6fdb5b284c5e6e2ea381ca36
Jadennl <jnleonard@hotmail.com>
2018-11-27 08:45:08 -0500

    Finished thanksgiving work

--- Thanksgiving/src/Thanksgiving.java
+++ Thanksgiving/src/Thanksgiving.java
@@ -2,13 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Thanksgiving {
-    private static Map<String,Integer> foods = new TreeMap<>();
+    private static Map<String, Integer> foods = new TreeMap<>();
     public static void main(String[] args) {
         Scanner fileRead = null;
-        
         try {
             File f = new File("Thanksgiving/Foods.txt");
             fileRead = new Scanner(f);
+            printFreq(fileRead);
+            printPop( foods );
         }
         catch(FileNotFoundException e) {
         
@@ -23,7 +24,7 @@ public class Thanksgiving {
     public static void printFreq(Scanner s) {
         while (s.hasNext()) {
             String food = s.nextLine();
-            if (foods.containsKey( food ) == false ) {
+            if ( !foods.containsKey( food ) ) {
                 foods.put(food, 1);
             }
             else {
@@ -31,9 +32,29 @@ public class Thanksgiving {
             }
         }
          for (String food : foods.keySet()) {
-             System.out.printf( "%s: %d",food, foods.get(food) );
+             System.out.printf( "%-40s %d%n",food,foods.get(food) );
          }
     }
     
+    public static void printPop(Map<String, Integer> m) {
+        Map<Integer, TreeSet<String>> ratings = new TreeMap<>();
+        for (String food : m.keySet()) {
+            TreeSet<String> bucket = ratings.get(foods.get(food));
+            if ( bucket == null ) {
+                bucket = new TreeSet<String>();
+            }
+            bucket.add( food );
+            ratings.put( foods.get(food), bucket);
+        }
+        
+        for(int i : ratings.keySet()) {
+            StringBuilder list = new StringBuilder("[");
+            for (String s : ratings.get( i )) {
+                list.append( s + ", ");
+            }
+            list.replace(list.length() - 2, list.length(), "]" );
+            System.out.printf( "%-10d %s%n", i, list );
+        }
+    }
     
 }
