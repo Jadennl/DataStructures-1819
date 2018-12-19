596021a74d33cd8c961b32b477dd1a7af327f70c
Jadennl <jnleonard@hotmail.com>
2018-12-19 12:30:33 -0500

    Added XMLReader project

---
+++ XmlReader/src/XmlReader.java
@@ -0,0 +1,21 @@
+public class XmlReader {
+    public static void main( String [] args ) {
+        String contents = getFileContents( "./src/strings.xml" );
+       // String answer = do a lot of work before getting here;
+        writeFileContents("./src/translateStrings.txt");
+    }
+    
+    private static String getFileContents( String filename )
+            throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filename)));
+    }
+    
+    private static void writeFileContents( String filename, String data )
+            throws IOException {
+        FileWriter fw = new FileWriter(new File(filename));
+        fw.write( data );
+        fw.close();
+    }
+    
+    
+}
